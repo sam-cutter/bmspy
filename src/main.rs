7d133cb124d24e9944a034c8a4d02be571296eb8
Sam Cutter <samuelcutter27@gmail.com>
2024-02-10 22:56:46 +0000

    Moved product-related structs and enums into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
 fn main() {
     println!("Hello, world!");
 }
-
-struct Product {
-    id: String,
-    back_market_uuid: String,
-    back_maket_slug: String,
-    title: String,
-}
-
-struct Snapshot {
-    id: String,
-    product: Product,
-    grade: Grade,
-    availability: Availability,
-}
-
-enum Availability {
-    Available(f64),
-    NotAvailable,
-}
-
-enum Grade {
-    Fair,
-    Good,
-    Excellent,
-}
---
+++ src/product/mod.rs
@@ -0,0 +1,24 @@
+pub struct Product {
+    pub id: String,
+    pub back_market_uuid: String,
+    pub back_maket_slug: String,
+    pub title: String,
+}
+
+pub struct Snapshot {
+    pub id: String,
+    pub product: Product,
+    pub grade: Grade,
+    pub availability: Availability,
+}
+
+pub enum Availability {
+    Available(f64),
+    NotAvailable,
+}
+
+pub enum Grade {
+    Fair,
+    Good,
+    Excellent,
+}
