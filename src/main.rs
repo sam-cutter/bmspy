efdd246a62f5bf60c420fd36e332e8511377ca62
Sam Cutter <samuelcutter27@gmail.com>
2024-02-10 18:35:34 +0000

    Created basic structs and enums for products, snapshots and grades

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Product {
+    id: String,
+    back_market_uuid: String,
+    back_maket_slug: String,
+    title: String,
+}
+
+struct ProductSnapshot {
+    id: String,
+    product: Product,
+    grade: Grade,
+    available: bool,
+    price: Option<f64>,
+}
+
+enum Grade {
+    Fair,
+    Good,
+    Excellent,
+}
