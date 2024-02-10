d9119ca2bf16facb6d0bac53ab1f0fc4ddae9c26
Sam Cutter <samuelcutter27@gmail.com>
2024-02-10 23:10:55 +0000

    Removed more unnecessary fields in the Product and Snapshot structs

--- src/product/mod.rs
+++ src/product/mod.rs
@@ -1,11 +1,9 @@
 pub struct Product {
-    pub id: String,
     pub back_market_uuid: String,
     pub title: String,
 }
 
 pub struct Snapshot {
-    pub id: String,
     pub product: Product,
     pub grade: Grade,
     pub availability: Availability,
