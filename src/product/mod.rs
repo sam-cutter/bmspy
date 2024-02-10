448911ef2bc49de4136e5f1a2bd775b19af6cb16
Sam Cutter <samuelcutter27@gmail.com>
2024-02-10 23:18:14 +0000

    Templated a constructor function for Product

--- src/product/mod.rs
+++ src/product/mod.rs
@@ -3,6 +3,20 @@ pub struct Product {
     pub title: String,
 }
 
+impl Product {
+    pub fn new(url: &str) -> Result<Self, &str> {
+        // TODO: Extract the Back Market UUID from the product URL
+        let back_market_uuid = String::new();
+        // TODO: Fetch the product title
+        let title = String::new();
+
+        Result::Ok(Product {
+            back_market_uuid,
+            title,
+        })
+    }
+}
+
 pub struct Snapshot {
     pub product: Product,
     pub grade: Grade,
