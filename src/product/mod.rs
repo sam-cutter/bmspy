08fcf7f68ed9fe77d4003673788c6ff3c18e6a00
Sam Cutter <samuelcutter27@gmail.com>
2024-02-13 15:54:26 +0000

    Updated control flow of extract_back_market_uuid for clearer reading

--- src/product/mod.rs
+++ src/product/mod.rs
@@ -20,10 +20,23 @@ impl Product {
     }
 }
 
-fn extract_back_market_uuid(url: &str) -> Result<String, &str> {
+pub fn extract_back_market_uuid(url: &str) -> Result<String, &str> {
     // TODO: Standardise error messages
     // TODO: Move to a separate module
 
+    // Ensure that the URL is valid
+    let url = match url::Url::parse(url) {
+        Ok(url) => url,
+        Err(_) => return Err("Unable to interpret product URL."),
+    };
+
+    // Ensure that the URL contains a hostname
+    let host = match url.host() {
+        Some(host) => host.to_string(),
+        None => return Err("Invalid product URL hostname."),
+    };
+
+    // Accepted Back Market hostnames
     const VALID_HOSTNAMES: [&str; 24] = [
         "www.backmarket.co.uk",
         "backmarket.co.uk",
@@ -51,38 +64,35 @@ fn extract_back_market_uuid(url: &str) -> Result<String, &str> {
         "backmarket.fi",
     ];
 
-    let url = match url::Url::parse(url) {
-        Ok(url) => url,
-        Err(_) => {
-            return Err("Unable to interpret product URL.");
-        }
-    };
-
-    if !VALID_HOSTNAMES.contains(&url.host_str().unwrap_or("")) {
+    // If the hostname is not a valid Back Market hostname, return an error
+    if !VALID_HOSTNAMES.contains(&host.as_str()) {
         return Err("Invalid product URL hostname.");
     }
 
+    // Collect the URL path segments
     let url_path_segments: Vec<&str> = match url.path_segments() {
-        Some(segments) => segments.filter(|segment| !segment.is_empty()).collect(),
+        Some(segments) => segments.collect(),
         None => return Err("Invalid product URL path."),
     };
 
+    // Ensure that the URL matches the pattern of /{locale}/p/{slug}/{uuid}
     if url_path_segments.len() != 4 || url_path_segments[1] != "p" {
         return Err("Invalid product URL pattern.");
     }
 
     let back_market_uuid = url_path_segments[3];
 
+    // Regex of a UUID
     let uuid_regex = regex::Regex::new(
         r"^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
     )
     .unwrap();
 
-    if !uuid_regex.is_match(back_market_uuid) {
-        return Err("Invalid product UUID.");
+    // Ensure that the UUID matches the UUID regex
+    match uuid_regex.is_match(back_market_uuid) {
+        true => return Ok(back_market_uuid.to_string()),
+        false => return Err("Invalid product UUID."),
     }
-
-    Ok(back_market_uuid.to_string())
 }
 
 pub struct Snapshot {
