264d44da2e43455bed39e7d206058100f7a81e9a
Sam Cutter <samuelcutter27@gmail.com>
2024-02-20 21:48:02 +0000

    Implemented basic title fetching for new products, and created a test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use product::Product;
+
 mod product;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let url = "https://www.backmarket.co.uk/en-gb/p/macbook-air-2020-13-inch-with-m1-8-core-and-7-core-gpu-8gb-ram-ssd-2tb-qwerty-english-us/b5ebc79d-0304-41a6-b1ae-d2a487afa11f#l=12";
+    match Product::from_url(url).await {
+        Ok(product) => {
+            println!("Back Market UUID: {}", product.back_market_uuid());
+            println!("Title: {}", product.title());
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+        }
+    }
 }
--- src/product/mod.rs
+++ src/product/mod.rs
@@ -2,6 +2,7 @@ mod back_mareket_uuid_extractor;
 mod snapshot;
 
 use back_mareket_uuid_extractor::extract_back_market_uuid_from_url;
+use serde_derive::Deserialize;
 pub use snapshot::Snapshot;
 
 pub struct Product {
@@ -9,15 +10,46 @@ pub struct Product {
     title: String,
 }
 
+#[derive(Deserialize)]
+struct ApiResponse {
+    title: String,
+}
+
 impl Product {
-    pub fn from_url(url: &str) -> Result<Self, String> {
+    pub async fn from_url(url: &str) -> Result<Self, String> {
         let back_market_uuid = match extract_back_market_uuid_from_url(url) {
             Ok(uuid) => uuid,
             Err(e) => return Err(e.message().to_string()),
         };
 
-        // TODO: Fetch the product title
-        let title = String::new();
+        let api_url = format!(
+            "https://www.backmarket.co.uk/bm/product/{}/technical_specifications",
+            back_market_uuid
+        );
+
+        let client = match reqwest::Client::builder()
+            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537")
+            .build() {
+            Ok(client) => client,
+            Err(_) => return Err("Failed to create client".to_string()),
+        };
+
+        let response = match client
+            .get(&api_url)
+            .header("Accept-Language", "en-gb")
+            .send()
+            .await
+        {
+            Ok(res) => res,
+            Err(_) => return Err("Failed to send request".to_string()),
+        };
+
+        let api_response: ApiResponse = match response.json().await {
+            Ok(json) => json,
+            Err(_) => return Err("Failed to parse JSON".to_string()),
+        };
+
+        let title = api_response.title;
 
         Result::Ok(Product {
             back_market_uuid,
