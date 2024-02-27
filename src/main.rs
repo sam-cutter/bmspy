c18153144424954260c1b9faa29c817b1f639254
Sam Cutter <samuelcutter27@gmail.com>
2024-02-27 07:46:19 +0000

    Added the sqlx crate instead of diesel

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ mod product;
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+    let databsae_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = sqlx::PgPool::connect(&databsae_url).await.unwrap();
+
     let url = "https://www.backmarket.co.uk/en-gb/p/macbook-air-2020-13-inch-with-m1-8-core-and-7-core-gpu-8gb-ram-ssd-2tb-qwerty-english-us/b5ebc79d-0304-41a6-b1ae-d2a487afa11f#l=12";
     match Product::from_url(url).await {
         Ok(product) => {
