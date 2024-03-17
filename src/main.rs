d0ea0bd3bcfc99e3f05501a517988e2c7bbe2f21
Sam Cutter <sam@samcutter.com>
2024-03-17 22:01:06 +0000

    Added database connection logic

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ mod product;
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = sqlx::PgPool::connect(&database_url).await.expect("Failed to connect to pool.");
+    
     let url = "https://www.backmarket.co.uk/en-gb/p/macbook-air-2020-13-inch-with-m1-8-core-and-7-core-gpu-8gb-ram-ssd-2tb-qwerty-english-us/b5ebc79d-0304-41a6-b1ae-d2a487afa11f#l=12";
     match Product::from_url(url).await {
         Ok(product) => {
