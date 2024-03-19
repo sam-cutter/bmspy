1ed600a1628b6c22fc31a24ac7c24eee72e88e8a
Sam Cutter <admin@samcutter.com>
2024-03-19 20:14:32 +0000

    Updated migration name to be valid, added code to run migrations

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ async fn main() {
 
     let pool = sqlx::PgPool::connect(&database_url).await.expect("Failed to connect to pool.");
     
+    sqlx::migrate!("./migrations").run(&pool).await.expect("Failed to run migrations");
+    
     let url = "https://www.backmarket.co.uk/en-gb/p/macbook-air-2020-13-inch-with-m1-8-core-and-7-core-gpu-8gb-ram-ssd-2tb-qwerty-english-us/b5ebc79d-0304-41a6-b1ae-d2a487afa11f#l=12";
     match Product::from_url(url).await {
         Ok(product) => {
